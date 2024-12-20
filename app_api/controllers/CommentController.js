44ea23acccbfa8faa1f00840ac79fc192df2dc61
Yusuf Eren Karabulut <erenikikarabulut@gmail.com>
2024-12-20 17:59:31 +0300

    added

---
+++ app_api/controllers/CommentController.js
@@ -0,0 +1,135 @@
+var mongoose=require("mongoose");
+var Venue=mongoose.model("venue");
+
+const calculateLastRating = function(incomingVenue, isDeleted) {
+    var i, numRatings, avgRating, sumRating = 0;
+    var numComments = incomingVenue.comments.length;
+    if(incomingVenue.comments) {
+            if(incomingVenue.comments.length == 0 && isDeleted) {
+                avgRating = 0;
+            }
+            else {
+                for(i=0; i<numComments; i++) {
+                    sumRating = sumRating + incomingVenue.comments[i].rating;
+                }
+                avgRating = Math.ceil(sumRating / numComments);
+            }
+            incomingVenue.rating = avgRating;
+            incomingVenue.save();
+    }
+};
+
+const updateRating = function(venueid, isDeleted) {
+    Venue.findById(venueid).select("rating comments")
+    .exec().then(function(venue) {
+        calculateLastRating(venue.isDeleted);
+    })
+};
+
+const createComment = function(req, res, incomingVenue) {
+    incomingVenue.comments.push(req.body);
+    incomingVenue.save().then(function(venue) {
+        var comments = venue.comments;
+        var comment = comments[comments.length-1];
+        updateRating(venue._id, false);
+        createResponse(res, "201", comment);
+    });
+};
+
+const createResponse=function(res,status,content) {
+    res.status(status).json(content);
+};
+
+const addComment=async function(req,res) {
+    try {
+        await Venue.findById(req.params.venueid)
+        .select("comments")
+        .exec()
+        .then(function(venue) {
+            createComment(req, res, venue);
+        })
+    }
+
+    catch(error) {
+        createResponse(res, "400", error);
+    }
+};
+
+const deleteComment=async function(req,res) {
+    try {
+        await Venue.findById(req.params.venueid)
+        .select("comments")
+        .exec()
+        .then(function(venue) {
+            try {
+                let comment = venue.comments.id(req.params.commentid);
+                comment.deleteOne();
+                venue.save().then(function() {
+                    updateRating(venue._id, true);
+                    createResponse(res, "200", {status: comment.author+ " isimli kişinin yorumu silindi."})
+                });
+            } catch (error) {
+                createResponse(res, "400", error);
+            }
+        });
+    } catch(error) {
+        createResponse(res, "400", error)
+    }
+};
+
+const updateComment=async function(req,res) {
+    try {
+        await Venue,findById(req.params.venueid)
+        .select("comments")
+        .exec()
+        .then(function(venue) {
+            try {
+                let comment = venue.comments.id(req.params.commentid);
+                comment.set(req.body);
+                venue.save().then(function() {
+                    updateRating(venue._id, false);
+                    createResponse(res, "201", comment);
+                });
+            } catch(error) {
+                createResponse(res, "400", error);
+            }
+        });
+    } catch (error) {
+        createResponse(res, "400", error);
+    }
+};
+
+const getComment = async function (req, res) {
+    try {
+        await Venue.findById(req.params.venueid)
+            .select("name comments").exec().then(function (venue) {
+                var response, comment;
+
+                if (!venue) {
+                    createResponse(res, "404", "Mekanid yanlış");
+                } else if (venue.comments.id(req.params.commentid)) {
+                    comment = venue.comments.id(req.params.commentid);
+                    response = {
+                        venue: {
+                            name: venue.name,
+                            id: req.params.id,
+                        },
+                        comment: comment,
+                    };
+                    createResponse(res, "200", response);
+                } else {
+                    createResponse(res, "404", "Yorum id yanlış");
+                }
+            });
+    } catch (error) {
+        createResponse(res, "404", "Mekan bulunamadı");
+    }
+};
+
+
+module.exports={
+    getComment,
+    addComment,
+    updateComment,
+    deleteComment
+};
---
+++ app_api/models/db.js
@@ -0,0 +1,17 @@
+var mongoose=require('mongoose');
+//Cloud mongodb için aşağıdaki adresi cloud adresiyle değiştirin.
+var dbURI="mongodb+srv://yusuferen:YUZAR1sieff@cluster0.iifxl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+mongoose.connect(dbURI);
+mongoose.connection.on("connected",function(){
+    console.log(dbURI+" adresine bağlandı");
+});
+mongoose.connection.on("disconnected",function(){
+    console.log(dbURI+" bağlantısı koptu");
+});
+
+process.on("SIGINT",function(){
+    mongoose.connection.close();
+    console.log("Bağlantı kapatıldı");
+    process.exit(0);
+});
+require("./venue");
